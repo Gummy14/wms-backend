5183e2bb1e2e7b5ee2de537f7efc540cdd67429e
Gummy14 <alex.helm@yahoo.com>
2024-11-15 20:04:17 -0500

    Now Picking Into Container

--- WMS API/obj/Debug/net8.0/WMS API.AssemblyInfo.cs
+++ WMS API/obj/Debug/net8.0/WMS API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMS API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d531622d4423ef5cd4165f3a9158c347c419225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4592af6b98b57fabd75b963716a19a5e7933d1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMS API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMS API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
